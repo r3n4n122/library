13d9c33c0c413dd8fe099a742ee6c03487b78590
r3n4n122 <renan.goncalves@3clicksrh.com.br>
2025-11-18 22:52:54 -0300

    created a table to books

---
+++ backend/db/migrate/20251119014930_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :isbn
+      t.integer :page_number
+      t.integer :year
+      t.json :raw_data, default: {}
+      t.timestamps
+    end
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_19_014930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "isbn"
+    t.integer "page_number"
+    t.integer "year"
+    t.json "raw_data", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
